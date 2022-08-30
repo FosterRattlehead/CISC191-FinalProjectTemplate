1aa9aae2581ccf6b6918fd491b292c8c8cc45206
Emad Shiran <emad534624@gmail.com>
2022-08-29 23:29:05 -0700

    Testing

---
+++ Server/src/main/java/edu/sdccd/cisc191/template/textEx.java
@@ -0,0 +1,28 @@
+package edu.sdccd.cisc191.template;
+
+public class textEx {
+
+    private String name;
+    private int birthYear;
+
+    public textEx(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+}
